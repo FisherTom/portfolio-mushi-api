939a73c69a7188a11e4eb5b427a08f01ff0d0fc3
fishertom <tfisher92@gmail.com>
2023-02-21 16:21:17 +0000

    amend dev data

--- db/data/development-data/users.js
+++ db/data/development-data/users.js
@@ -1,17 +1,17 @@
 module.exports = [
   {
     username: "user1",
-    num_reports: 3,
+    num_reports: 0,
     avatar_url: "https://example.com/avatar1.jpg",
   },
   {
     username: "user2",
-    num_reports: 1,
+    num_reports: 0,
     avatar_url: "https://example.com/avatar2.jpg",
   },
   {
     username: "user3",
-    num_reports: 2,
+    num_reports: 0,
     avatar_url: "https://example.com/avatar3.jpg",
   },
 ];
