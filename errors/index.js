30d4cfc38fa83d5fb155f99f15727baaa0c218cd
Kyle Goh <KyleGoh@Kyles-MacBook-Pro-383.local>
2023-02-21 10:19:09 +0000

    handleRoutingError implemented

--- errors/index.js
+++ errors/index.js
@@ -1,3 +1,7 @@
+exports.handleRouteErrors = (request, response, next) => {
+  response.status(404).send({ msg: "Route Does Not Exist" });
+};
+
 exports.handleServerErrors = (error, request, response, next) => {
   console.log(error);
   response.status(500).send("Internal Server Error");
