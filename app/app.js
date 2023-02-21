2c1648975f24db21c0dc46cf5d1af06e33ff30b5
Kyle Goh <KyleGoh@kyles-mbp-383.home>
2023-02-21 01:44:16 +0000

    ready for more tests

--- app/app.js
+++ app/app.js
@@ -1,3 +1,12 @@
 const express = require("express");
-
 const app = express();
+
+const { handleServerErrors } = require("../errors/");
+
+const { getMushrooms } = require("../controllers/app-controllers");
+
+app.get("/api/mushrooms", getMushrooms);
+
+app.use(handleServerErrors);
+
+module.exports = app;
